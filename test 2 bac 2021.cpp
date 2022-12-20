481e208293b38b91003577977b5aa36cffccb377
jurcaade <115492064+jurcaade@users.noreply.github.com>
2022-12-20 16:50:43 +0200

    Update test 2 bac 2021.cpp

--- test 2 bac 2021.cpp
+++ test 2 bac 2021.cpp
@@ -1,1 +1,34 @@
 sub 1
+  1. a
+  2. f(4)--x=4>1 A
+           x=3 f(2)
+           x=2 f(1) tip 1
+           x=1 f(0) tip 0
+            tip 1
+                  f(2)--x=2>1 A
+                        x=1 f(0)-- tip 0
+                        tip 1
+  01101
+               d)
+  3.{„Bono” 1 , „Bruce Springsteen” 2 , „Bon Jovi” 3 , „Leonard Cohen” 4, „Portugal.The Man” 5}
+  {„Bono” 1 , „Bon Jovi” 3 , „Portugal.The Man” 5} (nu pe poz alaturate)
+ultima e: c) 5454
+  
+  4.b
+  5.b
+  
+  sub 2
+  1. a n=2186310
+  s=1
+  c1=0 n=218631 c2=1
+  0<1 A
+  s=-1
+  0-1*-1>0 A 218631>9 A
+  c1=1 n=21863 c2=3
+  c1=3 c2=6
+  c1=6 c2=8
+  c1=8 n=21 c2=1
+  8-1*-1>0 A
+  tip -1 21
+  
+  b) 12345 13456
