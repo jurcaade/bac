ae38452427d80921c94b4b8c598764ed30fb4de4
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-01-03 15:02:04 +0200

    Update test 3 bac 2021.cpp

--- test 3 bac 2021.cpp
+++ test 3 bac 2021.cpp
@@ -1,1 +1,13 @@
 sub 1
+  1.b
+  2. *54321*543*5*////
+ f(54321) tip * tip 54321 f(543) tip /
+                              = tip * tip 543 f(5) tip /
+                                                = tip * tip 5 f(0) tip /
+                                                                = tip * tip /
+  => c
+  3. (MAVA, MCA, MCFR, MNHCV, MNLR, MNT)
+ (MCA, MNHCV, MNLR, MNT)
+  b
+  4.a
+  5.b
