9578635f5a01eaf51466f22482e03442322b3e33
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-01-12 18:25:35 +0200

    Update test 6 bac 2021.cpp

--- test 6 bac 2021.cpp
+++ test 6 bac 2021.cpp
@@ -101,6 +101,30 @@ int main() {
     return 0;
 }
   
+2.#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,a[21][21];
+    cin>>n;
+    for (int i=1;i<=n;i++)
+     for (int j=n;j>=1;j--)
+ {        if (i+j==n+1)
+         a[i][j]=0;
+         if (i+j<n+1)
+         a[i][j]=a[i][j+1]+1;
+         if (i+j>n+1)
+         a[i][j]=a[i-1][j]+1;
+}
+     
+     for (int i=1;i<=n;i++)
+ {    for (int j=1;j<=n;j++)
+     cout<<a[i][j]<<" ";
+     cout<<endl;
+}
+    return 0;
+}
   
   
 
