2031f062c02f1faf1e0fc1c330db49f8eda7128d
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-01-17 17:39:29 +0200

    Update test 7 bac 2021.cpp

--- test 7 bac 2021.cpp
+++ test 7 bac 2021.cpp
@@ -82,7 +82,31 @@ else
   s=BDUED
   
   sub 3
-  
+  1. #include <iostream>
+
+using namespace std;
+void afisare (int x, int y, int k)
+{
+    int nr=0;
+    for (int i=x; i<=y; i++)
+   {
+       cout<<i<<" ";
+       nr++;
+    if (nr==k)
+        {
+        cout<<"*"<<" ";
+        nr=0;
+       }
+   }
+   cout<<"*";
+}
+int main() {
+    int x,y,k;
+    cin >> x>>y>>k;
+   afisare(x,y,k);
+    return 0;
+}
+
   
   
   
