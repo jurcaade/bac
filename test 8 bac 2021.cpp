0dc131f54c591ddc947a226046b8f800494c788a
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-01-20 10:19:23 +0200

    Update test 8 bac 2021.cpp

--- test 8 bac 2021.cpp
+++ test 8 bac 2021.cpp
@@ -4,3 +4,21 @@ sub 1
   3.b
   4.c
   
+sub 2
+  2. struct clasa
+  {
+    unsigned int numar;
+    struct FaraNume
+    {
+      float sem1,sem2;
+    }elev[40];
+  }p;
+
+3.s=voalata
+tip 7
+ s=volt
+ tip 7 volt
+ 
+ sub 3
+  1.
+      
