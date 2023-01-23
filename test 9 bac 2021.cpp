8deb2ac18862a15b6b976567b404f5845fd77594
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-01-23 17:59:00 +0200

    Update test 9 bac 2021.cpp

--- test 9 bac 2021.cpp
+++ test 9 bac 2021.cpp
@@ -69,4 +69,20 @@ d.
         for(j=0;j<5;j++)
         a[i][j]=(i+j)%3;
 
-  
+  sub 3
+    1.#include <iostream>
+
+using namespace std;
+void divizor(int a, int b, int k, int &nr)
+{
+    for (int i=a;i<=b;i++)
+    if (i%k==0 && i%10==k)
+    nr++;
+}
+int main() {
+  int a,b,k,nr=0;
+  cin>>a>>b>>k;
+  divizor(a,b,k,nr);
+  cout<<nr;
+    
+}
