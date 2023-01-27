92dd8ff6ff2785fee794824eabc3e657c4d97006
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-01-27 10:50:48 +0200

    Update test 10 bac 2021.cpp

--- test 10 bac 2021.cpp
+++ test 10 bac 2021.cpp
@@ -10,5 +10,67 @@ sub 1
                       tip 12             0>0 F
                       tip +              tip +
   
-3.
+3. b
+4. d
+5. c
+
+sub 2.
+1. a. x=2 y=9
+2<9 A
+x=-7 y=2 x=9
+9>=2 A
+tip A
+x=7
+tip B
+tip A
+tip B
+tip A
+tip B
+tip A
+tip B
+1>=2 F
+tip ABABABAB
+
+b. 13, 14
+
+c. #include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int x,y;
+    cin>>x>>y;
+    if (x<y)
+    {
+        x=x-y;
+        y=x+y;
+        x=y-x;
+    }
+    
+    while (x>=y)
+    {
+        cout<<'A';
+        x=x-y;
+        if (x%2==0)
+        cout<<'A';
+        else
+        cout<<'B';
+        
+    }
+    return 0;
+}
+
+d.citeşte x,y (numere naturale)
+┌dacă x<y atunci
+│ xx-y; yx+y; xy-x
+└■
+┌repeta 
+│ scrie ’A’
+│ xx-y
+│┌dacă x%2=0 atunci scrie ’A’
+││altfel scrie ’B’
+│└■
+|pana cand x<y
+└■
+
   
