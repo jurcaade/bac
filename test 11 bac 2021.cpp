2d32f36eb30ccc983f0020111e1b0a4495cd9397
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-02-21 16:55:38 +0200

    Update test 11 bac 2021.cpp

--- test 11 bac 2021.cpp
+++ test 11 bac 2021.cpp
@@ -1,2 +1,59 @@
 sub 1
-  1.
+  1.c
+  2.b
+  3.b
+  4.c
+  5.d
+ 
+  
+  sub 2
+  a. m=2
+  n=9
+  m=3
+  m=5 *
+  m=7 *
+  m=9 *
+  m=11 *
+  tip ****
+  b 2,3
+  c.
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int m,n;
+    cin>>m>>n;
+    int aux;
+    if (m>n)
+    {
+        aux=m;
+        m=n;
+        n=aux;
+    }
+    
+    if (m%2==0)
+    m=m+1;
+    
+    while (m<=n)
+    {
+        m+=2;
+        cout<<"*";
+    }
+    return 0;
+}
+d. citește m,n
+ (numere naturale)
+┌dacă m>n atunci
+│ n→m
+└■
+┌dacă m%2=0 atunci
+│ mm+1
+└■
+
+repeta
+│mm+2
+│ scrie ‘*’
+pana cand m>n
+
+
