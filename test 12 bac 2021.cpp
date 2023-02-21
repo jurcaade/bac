a240b3e8f15e1d0e5aa7c5b4041668ba80e1b8fa
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-02-21 20:10:36 +0200

    Update test 12 bac 2021.cpp

--- test 12 bac 2021.cpp
+++ test 12 bac 2021.cpp
@@ -31,3 +31,32 @@ int main() {
     cout<<y;
     return 0;
 }
+
+2.
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,k, a[21][21];
+    cin>>n>>k;
+    for (int i=1;i<=n;i++)
+    for (int j=1;j<=n;j++)
+    cin>>a[i][j];
+    
+    int aux;
+    aux=a[k][n-k];
+    
+    for (int j=n-k;j>=2;j--)
+    a[k][j]=a[k][j-1];
+
+a[k][1]=aux;
+
+     for (int i=1;i<=n;i++)
+ {   for (int j=1;j<=n;j++)
+    cout<<a[i][j]<<" ";
+    cout<<endl;
+}
+    
+    return 0;
+}
