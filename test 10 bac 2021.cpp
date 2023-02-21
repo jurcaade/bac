dfd3b93fd9a7bd56e6a23c9e1d0aa310d006f6a6
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-02-21 15:33:29 +0200

    Update test 10 bac 2021.cpp

--- test 10 bac 2021.cpp
+++ test 10 bac 2021.cpp
@@ -90,7 +90,41 @@ t=pom
 tip pom
     verde
     
+    sub 3
+  1.
+  #include <iostream>
+
+using namespace std;
+
+int armonie(int x, int y)
+{
+    int s1=0,s2=0;
     
+    for (int d=1;d<=x;d++)
+    if (x%d==0)
+    s1+=d;
+    
+    
+    for (int d=1;d<=y;d++)
+    if (y%d==0)
+    s2+=d;
+    
+    if ((x+y)>s1 && (x+y)<s2)
+    return 1;
+    else
+    return 0;
+}
+
+
+
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    cout<<armonie(x,y);
+   
+    return 0;
+}
 
 
 
