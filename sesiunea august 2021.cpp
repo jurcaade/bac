08bd57612c80cf445862fe089cadcb34beaba9b9
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-02-28 16:31:17 +0200

    Update sesiunea august 2021.cpp

--- sesiunea august 2021.cpp
+++ sesiunea august 2021.cpp
@@ -90,7 +90,7 @@ int main() {
 }
 
 2.
-  #include <iostream>
+ #include <iostream>
 
 using namespace std;
 
@@ -98,11 +98,12 @@ int main() {
     int a[101][101],k,n;
     cin>>k>>n;
     
+     for(int i=1;i<=n;i++)
+     a[i][i]=i*k;
     
-    for(int i=1;i<=n;i++)
+     for(int i=1;i<=n;i++)
         for (int j=1;j<=n;j++)
-    {    if (i==j)
-        a[i][j]=a[i-1][j-1]+k;
+    {    
         if (i<j)
         a[i][j]=a[i][j-1]+1;
         if (i>j)
