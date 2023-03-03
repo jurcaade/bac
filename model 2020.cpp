4afba7542037b2012da6aa16265ad18f661627ac
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-03-03 10:44:05 +0200

    Update model 2020.cpp

--- model 2020.cpp
+++ model 2020.cpp
@@ -62,7 +62,42 @@ d. citește m,n,x
 
 sub 3
   1.
+#include <iostream>
 
+using namespace std;
+void duplicare (int n, int &d)
+{
+    int p=1,s=0,ok=0;
+    
+    while (n>0)
+    {
+        if ((n%10)%2!=0)
+        {
+        s=s+(n%10*10+n%10)*p;
+        p*=100;
+        ok=1;
+        }
+        else
+    {    s=s+n%10*p;
+        p*=10;
+    }
+     
+        n/=10;
+        
+    }
+    if (ok==1)
+    d=s;
+    else
+    d=-1;
+    
+}
+int main() {
+    int n,d;
+    cin>>n;
+    duplicare(n,d);
+    cout<<d;
+    return 0;
+}
 
 
 
