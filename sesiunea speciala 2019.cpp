6b4dc8cd578e0b188efb1bb191af4954a0d59877
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-03-11 13:03:26 +0200

    Update sesiunea speciala 2019.cpp

--- sesiunea speciala 2019.cpp
+++ sesiunea speciala 2019.cpp
@@ -11,3 +11,11 @@ sub 1
 
   2.
   d.luna < e.dev.luna || (d.luna == e.dev.luna && d.zi < e.dev.zi)
+
+  3.
+    for(i=1;i<=5;i++)
+     for(j=1;j<=5;j++)
+     if (i<=j)
+       a[i][j]=5-i;
+       else
+       a[i][j]=5-j;
