a129ed5d0f84d511a1c38c6cd2e37263478313ab
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-03-11 13:21:50 +0200

    Update sesiunea speciala 2019.cpp

--- sesiunea speciala 2019.cpp
+++ sesiunea speciala 2019.cpp
@@ -19,3 +19,36 @@ sub 1
        a[i][j]=5-i;
        else
        a[i][j]=5-j;
+
+sub 3
+  1.
+  #include <iostream>
+
+using namespace std;
+int Egal(int n)
+{
+    int pre=-1,x;
+    
+  
+    while (n>0)
+    {
+        x=n%10;
+        if (x%2!=0)
+        {
+         if (pre==-1)
+            pre=x;
+            else
+        if (pre!=x)
+        return 0;
+        }
+        
+       n/=10;
+    } 
+    return 1;
+}
+int main() {
+    int n;
+    cin>>n;
+   cout<<Egal(n);
+    return 0;
+}
