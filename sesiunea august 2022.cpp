2edbc72f4fee47e1208457efb4fc196cbf1262fe
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-03-14 08:39:01 +0200

    Update sesiunea august 2022.cpp

--- sesiunea august 2022.cpp
+++ sesiunea august 2022.cpp
@@ -1,2 +1,61 @@
 sub 1.
-  1.
+  1.b
+  2.d
+  3.d
+  4.i+j=n-1
+    i=n-1-j
+    i=100-1-20=79
+  c
+  5.b
+  
+  sub 2
+  1. a x=21 y=47 n=46
+  x=1 y=1
+  x=21 y=45 n=44
+  x=3 y=3
+  tip 45
+  
+  b. 10 11
+  c.#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned int m,n;
+    cin>>m>>n;
+    int x,y;
+    x=1;
+    while (x==1)
+    {
+        x=m; y=n; n=n-1;
+        while (x!=y)
+          {  if (x>y)
+            x=x-y;
+            else
+            y=y-x;
+    }
+    }
+    cout<<n+1;
+    return 0;
+}
+
+d.citeşte m,n
+ (numere naturale nenule, m<n)
+ x1
+┌cât timp x=1 execută
+│ xm; yn; nn-1
+│┌daca x≠y atunci
+││┌repeta
+│││┌dacă x>y atunci xx-y
+││││altfel yy-x
+│││└■
+││└pana cand (x=y)
+│└■
+└■
+scrie n+1
+  
+  
+  
+  
+  
+    
