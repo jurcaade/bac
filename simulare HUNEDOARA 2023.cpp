97f5adbace28432c916867ec5db7e8233cd3dda1
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-03-22 18:38:57 +0200

    Update simulare HUNEDOARA 2023.cpp

--- simulare HUNEDOARA 2023.cpp
+++ simulare HUNEDOARA 2023.cpp
@@ -1,1 +1,35 @@
+sub 3
+  ex 1
+  #include <iostream>
 
+using namespace std;
+
+int nrdiv(int n)
+{
+     int nr=0;
+    for (int d=1;d<=n;d++)
+    if (n%d==0)
+     {  
+         nr++;
+    }
+    return nr;
+}
+void divizorik(int n, int k)
+{
+    int i=n+1;
+   while (k>0)
+   {
+       
+       if (nrdiv(i)==nrdiv(n))
+    {   cout<<i<<" ";
+       k--;
+    }
+    i++;
+   }
+}
+int main() {
+    int n,k;
+    cin>>n>>k;
+    divizorik(n,k);
+    return 0;
+}
