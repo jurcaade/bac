ee42287896929a2179194a76fb112066712b1e21
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-04-20 11:59:28 +0300

    Update bac 2019 model.cpp

--- bac 2019 model.cpp
+++ bac 2019 model.cpp
@@ -1,1 +1,31 @@
-
+sub 1
+  1.d
+  2.a
+  3. {agar, geranium, iasomie, paciuli, tuberoze}
+inainte : agar paciuli tuberoze => b
+(geranium, iasomie, paciuli)
+  4. 1,2,3,4,8,10 => 6 noduri de tip frunza
+  => c
+  5.d
+  
+sub 2
+  1. 
+  a
+  n=7 k=1 7>1 i=1
+     n=6 
+     i=1 tip 1
+     i=0
+     k=2
+     6>2 A i=2
+     n=4
+     tip 2 2
+     k=3
+     4>3 A i=3
+     n=1 tip 3 3 3
+  k=4
+     1>4 F i=1
+      n=0
+      tip 4
+  1 2 3 3 3 4
+  
+  b 46 55
