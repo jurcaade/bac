2c1e5982787a0dbb34aee2a62f7b54e8dfbe48d2
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-04-26 19:02:30 +0300

    Update simulare iasi 2022.cpp

--- simulare iasi 2022.cpp
+++ simulare iasi 2022.cpp
@@ -1,2 +1,33 @@
 sub 1
-  1.
+  1 c
+  2 b
+  3 b
+  4 b
+  
+  sub 2
+ 1 d.
+  citeste n (număr
+natural nenul)
+pentru i←1, n execută
+| j←1
+|daca (j<=i) atunci
+│executa     /// repeta
+││dacă i%2=0 atunci
+||| scrie j
+│││ altfel scrie j*j
+││└■
+||   j←j+1
+│└■cat timp j<=i /// pana cand j>i
+└■ 
+2. struct vauto{
+  unsigned int km, pret;
+  float consum;
+}m[300];
+
+3.for (i=0; i<6; i++)
+ for (j=0; j<4; j++) 
+   if (j%2==0)
+     a[i][j]='A'+i;
+  else
+     a[i][j]='a'+i; 
+//a[i][j]='A'+(j%2)*32+i;
