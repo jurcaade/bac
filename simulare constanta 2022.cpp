58c4071f2a1d0e0c51d8d514ca6b15868226ef08
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-04-27 11:52:45 +0300

    Update simulare constanta 2022.cpp

--- simulare constanta 2022.cpp
+++ simulare constanta 2022.cpp
@@ -4,3 +4,40 @@ sub 1
   3.b
   4.c
   5.a
+
+  sub 2
+  1.a 1203
+    b 23432
+  c
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,m,p,u;
+    cin>>n;
+    m=0; p=1;
+    while (n>0)
+    {
+        u=n%10;
+        if (u<=3)
+   {     m=u*p+m;
+        p*=10;
+    }
+    n/=10;
+    }
+    cout<<m;
+    return 0;
+}
+
+d.
+  Citește n (numar natural)
+m0; p1;
+executa
+ un%10
+ dacă u<=3 atunci mu*p+m
+ pp*10
+cat timp n>0
+ n[n/10]
+
+Scrie m;
