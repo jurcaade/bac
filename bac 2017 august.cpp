d8dc3d3c88e16e3b10fe92777d139d159d13b301
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-05-20 15:32:28 +0300

    Update bac 2017 august.cpp

--- bac 2017 august.cpp
+++ bac 2017 august.cpp
@@ -1,1 +1,35 @@
+sub 1
+  1. a
+  2. a) x=4 c=2 s=2
+        x=5 c=5 s=7
+        x=6 c=2 s=9
+        x=7 c=7 s=16
+  tip 16
+  b) 
+  x=2 c=2
+  x=3 c=3 
+  x=4 c=2 
+  x=5 c=5 
+  x=6 c=2 
+  x=7 c=7 
+  x=8 c=2
+  x=9 c=3
+perechile: 2,4; 4,2; 4,5; 5,4; 5,6; 6,5; 7,7;
 
+c)citeşte a,b
+ (numere naturale, a≥2, b≥2)
+┌dacă a>b atunci
+│ xa
+│ ab
+│ bx
+└■
+ s←0
+┌pentru x←a,b execută
+│ c←2
+| daca x%c>0 atunci
+│┌repeta
+││ c←c+1
+│└pana cand x%c=0
+│ss+c
+└■
+scrie s 
