ca5be73f4cc9f75ef25d92bd18ef5f8e72d8fad1
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-05-22 12:11:09 +0300

    Update bac 2017 iunie.cpp

--- bac 2017 iunie.cpp
+++ bac 2017 iunie.cpp
@@ -1,1 +1,39 @@
+sub 1
+  1.a
+  2.n=3
+  a) #2#1#3#2#
+b)
+ pt n=10 se afiseaza 19 #
+ pt n=51 se afiseaza 100 #
+ 51
+ c)citeşte n
+ (număr natural nenul)
+  i<-1
+┌cat timp 1<=n execută
+│┌pentru j1,n execută
+││┌dacă i=j sau i+j=n+1 atunci
+│││ scrie ’#’
+│││altfel
+│││ scrie j
+││└■
+│└■
+ i<-i+1;
+└■
 
+d)
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    for (int j=1;j<=n;j++)
+    if (i==j || i+j==n+1)
+    cout<<'#';
+    else
+    cout<<j;
+    return 0;
+}
+ 
