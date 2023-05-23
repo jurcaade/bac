92b833e18ed4cff86a73cf8864e94301f01dc9e1
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-05-23 14:49:57 +0300

    Update bac 2017 iunie.cpp

--- bac 2017 iunie.cpp
+++ bac 2017 iunie.cpp
@@ -49,4 +49,35 @@ cout<<float(x.media1+x.media2)/2;
   00100
   00010
   
+  5.
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,m,a[51][51];
+    cin>>n>>m;
+   
+    
+    for (int i=1;i<=n;i++)
+    for (int j=1;j<=m;j++)
+    cin>>a[i][j];
+     int x=a[n][m];
+     
+     
+     for (int i=1;i<=n;i++)
+     a[i][1]=a[i][m]=x;
+     for (int j=1;j<=m;j++)
+     a[1][j]=a[n][j]=x;
+    
+    for (int i=1;i<=n;i++)
+ {   for (int j=1;j<=m;j++)
+    cout<<a[i][j]<<" ";
+    cout<<endl;
+}
+    
+    
+    return 0;
+}
+  
 
