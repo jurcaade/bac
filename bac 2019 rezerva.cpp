c15552cc31a8b1b6391fcbbc43b652c96cfde3dc
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-05-27 17:12:05 +0300

    Update bac 2019 rezerva.cpp

--- bac 2019 rezerva.cpp
+++ bac 2019 rezerva.cpp
@@ -130,7 +130,34 @@ int main() {
     return 0;
 }
 
-2.
+2.#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,a[101][101];
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    for (int j=1;j<=n;j++)
+    cin>>a[i][j];
+    
+  
+   for (int i=0;i<=n+1;i++)
+{    a[0][i]=a[n+1][i]=-1;
+   a[i][0]=a[i][n+1]=-1;
+}
+    
+    
+    
+    for (int i=1;i<=n;i++)
+    for (int j=1;j<=n;j++)
+    if (a[i][j]==3 && (a[i-1][j]==0 || a[i+1][j]==0 || a[i][j+1]==0 || a[i][j-1]==0))
+       cout<<i<<" ";
+    
+ 
+    
+    return 0;
+}
   
   
   
