9b9b733f9c1533b128508c9726e79df390d03074
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-05-28 14:21:03 +0300

    Update model 2023.cpp

--- model 2023.cpp
+++ model 2023.cpp
@@ -1,1 +1,42 @@
-
+sub 1.
+  1.b
+  2. f(20,26) tip 23
+  f21,24 tip 17
+  
+  f21,27=tip 24
+  f22,25 tip 23
+  f23,23 tip 23
+  =? d 
+  3. {castane, mousse, frișcă, șerbet, ganache}
+ultimul -frisca, ganache (nu pot fi pe primele) 
+4 (castane, șerbet, ganache)
+5 (mousse, castane, frisca)
+6 (mousse, castane, ganache)
+  => a
+  
+  4. c
+  5. d
+ 
+sub 2
+  1.a)
+  p=11 q=25
+  x=11 
+  x=12 s=12
+  x=12 s=0
+  x=15 s=15
+  x=16 s=31
+  x=18 s=49
+  x=20 s=69
+  x=21 s=90
+  x=24 s=104
+  x=24 s=90
+  tip 90
+  multiplii lui 3 si 4 de la 11 la 25 aduanti, fara cei comuni
+  b) intervalul 2,q
+  multiplii de 10 si 15
+  suma sa fie 250
+  M10={10,20,40,50,70,80,100
+  M15={15,45,75,105
+    deci pana la 70,71,72,73,74
+ 
+  
