4dba52dbaaf87700408af67d12cdcdeb87a1ffa9
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-03 17:47:36 +0300

    Update bac 2022 sesiunea speciala.cpp

--- bac 2022 sesiunea speciala.cpp
+++ bac 2022 sesiunea speciala.cpp
@@ -149,3 +149,42 @@ int main() {
 }
     return 0;
 }
+
+3.
+ #include <iostream>
+
+using namespace std;
+
+int main() {
+   int x,pre,lung=0,lmax=-1,s=0;
+   cin>>pre;
+   lung++;
+   while(cin>>x)
+   {
+       if (x!=pre)
+       {    if (lung==x)
+         {  s+=x;
+           lung=1;
+       }
+           else
+           { 
+              if (s>lmax)
+            {  lmax=s;
+                s=0;
+           }
+                lung=1;
+                s=0;
+           }
+       }
+       else
+       lung++;
+       
+       pre=x;
+   }
+   if (lung==x)
+   s+=x;
+   if(s>lmax)
+   lmax=s;
+   cout<<lmax;
+    return 0;
+}
