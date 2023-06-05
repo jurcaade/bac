f0109727cd37ccade31afe66059397a61d6a6841
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-05 16:29:54 +0300

    Update bac 2020 test 2.cpp

--- bac 2020 test 2.cpp
+++ bac 2020 test 2.cpp
@@ -74,3 +74,28 @@ scrie m
   unsigned int nrExemplare;
   unsigned int varsta[10];
 }s[20];
+
+
+3.#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a[101][101];
+    for (int i=0;i<5;i++)
+    for (int j=0;j<7;j++)
+    a[i][j]=1;
+    
+     for (int i=1;i<5;i++)
+    for (int j=1;j<7;j++)
+    a[i][j]=(a[i-1][j]+a[i][j-1])%10;
+    
+     for (int i=0;i<5;i++)
+ {   for (int j=0;j<7;j++)
+    cout<<a[i][j]<<" ";
+    cout<<endl;
+}
+    
+    
+    return 0;
+}
