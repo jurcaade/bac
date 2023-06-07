62ab68a688b5dd75bb1d1c68b107638ed27ad7b7
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-07 19:01:42 +0300

    Update bac 2023 sesiunea speciala.cpp

--- bac 2023 sesiunea speciala.cpp
+++ bac 2023 sesiunea speciala.cpp
@@ -103,11 +103,10 @@ using namespace std;
 int main() {
     int n,a[21][21],v[21];
     cin>>n;
-    for (int i=1;i<=n;i++)
-     cin>>v[i];
+  
      
      for (int j=1;j<=n;j++)
-     a[1][j]=v[j];
+     cin>>a[1][j];
      
       for (int i=2;i<=n;i++)
        for (int j=1;j<=n;j++)
