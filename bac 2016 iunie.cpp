914b2160f2d48e42c3b9dda55c826fba109a8847
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-08 19:31:18 +0300

    Update bac 2016 iunie.cpp

--- bac 2016 iunie.cpp
+++ bac 2016 iunie.cpp
@@ -39,4 +39,39 @@ int main() {
    }
    
    cout<<m<<" "<<n;
+  
 }
+sub 2
+  1.a
+  2. 6 elemente nenule => 3 muchii 
+  b
+  3. 2 3 8
+  4. s1=informatica
+     tip 11
+     s2=mate
+     s2=matematica
+     tip matematica
+     
+   5.#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,a[21][21];
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    a[i][1]=i;
+    
+     for (int i=n;i>=1;i--)
+        for (int j=2;j<=n;j++)
+    a[i][j]=a[i][j-1]+a[i+1][j-1];
+    
+    
+     for (int i=1;i<=n;i++)
+ {       for (int j=1;j<=n;j++)
+        cout<<a[i][j]<<" ";
+        cout<<endl;
+}
+    return 0;
+}
+   
