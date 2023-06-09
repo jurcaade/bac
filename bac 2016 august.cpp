4ace9c17d457c7463dce52363c1037a5209f69e2
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-09 17:05:04 +0300

    Update bac 2016 august.cpp

--- bac 2016 august.cpp
+++ bac 2016 august.cpp
@@ -1,1 +1,41 @@
+sub 1
+  1.a
+  2.a) p=12
+  b) 16,17
+  c) citeşte n
+ (număr natural nenul)
+ p0
+ i<-1
+┌repeta
+│ mi
+│┌cât timp m%2=0 execută
+││ m[m/2]
+│└■
+│┌dacă m=1 atunci
+││ pi
+│└■
+| i=i+1
+└pana cand i>n
+ scrie p
 
+d)
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,p,m;
+    cin>>n;
+    p=0;
+    for (int i=1;i<=n;i++)
+ {  
+     m=i;
+    while(m%2==0)
+    m/=2;
+    
+    if (m==1)
+    p=i;
+}
+
+cout<<p;
+}
