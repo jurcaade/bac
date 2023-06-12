cffd6540e615a8cc17ba7e2d1f79884c61b51ad4
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-12 14:37:53 +0300

    Update bac 2016 sesiunea speciala.cpp

--- bac 2016 sesiunea speciala.cpp
+++ bac 2016 sesiunea speciala.cpp
@@ -1,1 +1,47 @@
+sub 1
+  1.a
+  2. a) n=81112337  
+  tip 3
+  b) 11110, 11112, 12222
+  c) citeşte n
+ (număr natural nenul)
+ k1
+ m1
+ daca n>9 atunci
+┌repeta
+│┌dacă n%10=[n/10]%10 atunci
+││ kk+1
+││┌dacă k>m atunci
+│││ mk
+││└■
+││altfel
+││ k1
+│└■
+│ n[n/10]
+└pana cand n<=9
+ scrie m
 
+d)
+  #include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,k,m;
+    cin>>n;
+    k=1; m=1;
+    while(n>9)
+    {
+        if (n%10==n/10%10)
+     {   k++;
+        if (k>m)
+        m=k;
+    }
+    else
+    k=1;
+        n/=10;
+    }
+    cout<<m;
+
+    return 0;
+}
