dbd1baee3ead6bf1702aaa48a38642b877996a55
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-17 17:16:15 +0300

    Update bac 2020 test 5.cpp

--- bac 2020 test 5.cpp
+++ bac 2020 test 5.cpp
@@ -116,3 +116,33 @@ int main() {
          
     return 0;
 }
+
+
+sub 3
+  1.
+  #include <iostream>
+
+using namespace std;
+int baza(int n)
+{ 
+    int maxi=1;
+    
+    while(n>0)
+    {
+        if (n%10>maxi)
+        maxi=n%10;
+        
+        n/=10;
+    }
+    
+    return maxi+1;
+    
+}
+
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<baza(n);
+    return 0;
+}
