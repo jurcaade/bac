10bb86aca15baf48639408a4cfa9ebf4568d35dc
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-17 16:39:14 +0300

    Create bac 2020 test 5.cpp

---
+++ bac 2020 test 5.cpp
@@ -0,0 +1,27 @@
+sub 1.
+  1.a
+  2. 121131211212111
+  f(3) - f(2) 1211 tip 3 f(1) 1 tip 2 f(0) tip 1
+  f(2) f(1) tip 1211
+  f(1) f(0) tip 1
+  => a
+  3. {cămașă, cravată, pantaloni, pantofi, sacou, șosete}
+camasa <- cravata
+cravata <- sacou
+sosete, pantaloni <- pantofi
+
+prima :(cămașă, cravată, pantaloni, sacou, șosete, pantofi)
+a 2 a: (cămașă, cravată, pantaloni, șosete, pantofi, sacou)
+a 3 a: (cămașă, cravată, pantaloni, șosete, sacou, pantofi)
+a 4 a: (cămașă, cravată, sacou, pantaloni, sosete, pantofi) 
+a 5 a: (cămașă, cravată, sacou, șosete, pantaloni, pantofi)  
+a 6 a: (cămașă, cravată, șosete, pantaloni, pantofi, sacou)
+=> c
+
+4. b
+5.2020 noduri 200 elem nenule => 100 muchii
+ n(n-1)/2=m
+ n=15 folosim toate muchiile 
+ 2020-15=2005+1=2006
+  => a
+  
