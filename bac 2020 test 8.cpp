47e95b19d0470f33536d7ba9b8b2eae38c4dbeee
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-22 17:43:35 +0300

    Update bac 2020 test 8.cpp

--- bac 2020 test 8.cpp
+++ bac 2020 test 8.cpp
@@ -114,7 +114,7 @@ int main() {
     return 0;
 }
 
-2.
+2. la stanga
   #include <iostream>
 
 using namespace std;
@@ -137,6 +137,35 @@ int main() {
    
    }
     
+    for (int i=1;i<=n;i++)
+ {   for (int j=1;j<=n;j++)
+    cout<<a[i][j]<<" ";
+    cout<<endl;
+}
+    return 0;
+}
+
+la dreapta
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,k,a[21][21],aux=0;
+    cin>>n>>k;
+    for (int i=1;i<=n;i++)
+    for (int j=1;j<=n;j++)
+    cin>>a[i][j];
+    
+     aux=a[k][k-1];
+    for (int j=n;j>=1;j--)
+    if (k>j)
+   {
+       a[k][j]=a[k][j-1];
+       
+   }
+      a[k][1]=aux;
+      
     for (int i=1;i<=n;i++)
  {   for (int j=1;j<=n;j++)
     cout<<a[i][j]<<" ";
