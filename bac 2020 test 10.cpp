19ae073f16fdee53e9dc3232d497046b095ae11e
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-24 15:20:16 +0300

    Create bac 2020 test 10.cpp

---
+++ bac 2020 test 10.cpp
@@ -0,0 +1,18 @@
+sub 1.
+1. d
+2.  f(19,20)=f(18,21)=f(15,22)=f(8,23)=f(-7,24)=24
+  b
+3. {cal/,câine,papagal/,porumbel,ponei/}
+cal caine
+cal porumbel -- pt cuvant cu restrictii 2
+caine caine
+caine cal
+caine papagal
+caine porumbel
+caine ponei  -- pt cuvant fara restrictii sunt 5
+2+5+2+5+2=14
+  d
+4.a
+5.a
+
+  sub 2
