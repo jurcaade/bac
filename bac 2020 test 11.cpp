fe5354fe40af47f60a002d5bbe3806a34c0b195a
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-27 20:17:41 +0300

    Update bac 2020 test 11.cpp

--- bac 2020 test 11.cpp
+++ bac 2020 test 11.cpp
@@ -127,3 +127,31 @@ int main() {
     return 0;
 }
 
+
+2.
+  #include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char s[101],t[101]={};
+    cin.getline(s,101);
+    char *p=strtok(s," ");
+    
+    while(p!=NULL)
+    {
+        if (p[0]>='a' && p[0]<='z' )
+        p[0]-=32;
+        
+        for (int i=1;i<strlen(p);i++)
+         if (p[i]>='A' && p[i]<='Z' )
+        p[i]+=32;
+        
+        strcat(t,p);
+          strcat(t," ");
+        p=strtok(NULL," ");
+    }
+   strcpy(s,t);
+   cout<<s;
+    return 0;
+}
