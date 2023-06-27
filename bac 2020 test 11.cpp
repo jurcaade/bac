3f138f53fa75e1c3f863aa61d1f0d47ca6ba8d87
jurcaade <115492064+jurcaade@users.noreply.github.com>
2023-06-27 18:15:12 +0300

    Update bac 2020 test 11.cpp

--- bac 2020 test 11.cpp
+++ bac 2020 test 11.cpp
@@ -1,3 +1,16 @@
 sub 1
 1.d
 2. {azalea/, begonia/, vriesea/, busuioc, ferigă}
+a 6 a: (begonia, busuioc, vriesea)
+a 7 a: (begonia, feriga, azalea)
+a 8 a: (begonia, feriga, vrisea)
+a 9 a: (vrisea, busuioc, azalea)
+  => d
+  3.f2020,2=1+f1010,2=2022
+  f1010,2=1+f505,2=2021
+  f505,2=2020
+  => a
+
+  4.b
+  5. ne trb 5 noduri sa folosim toate muchiile
+restul nodurilor componente conexe=> 16 c)
